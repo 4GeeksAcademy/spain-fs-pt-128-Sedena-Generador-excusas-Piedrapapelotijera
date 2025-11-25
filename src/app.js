ae6c93ba579f058beb311f3ed251f15adcb798f3
Pablo Serrano García <disciplineart17@gmail.com>
2025-11-25 23:18:42 +0000

    Piedra papel o tijeras

--- src/app.js
+++ src/app.js
@@ -1,11 +1,72 @@
-import "bootstrap";
-import "./style.css";
+
+const opciones = ["scissors","lizard","spock","rock","paper"]
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const rules = {
+  rock: ['scissors', "lizard"],
+  paper: ["rock", "spock"],
+  scissors: ["paper", "lizard"],
+  lizard: ["spock", "paper"],
+  spock: ["scissors", "rock"]
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+function jugar (Usuario) {
+
+
+const OpciónOrdenador = Math.floor(Math.random() * opciones.length);
+
+let OrdenadorAString = opciones[OpciónOrdenador]
+
+
+if (!opciones.includes(Usuario)) {
+
+   console.log("Elige en base a las opciones dadas");
+
+
+} else if (rules[Usuario].includes(OrdenadorAString) ) {
+
+    console.log("Tu opción: "+ Usuario);
+
+    console.log("Elección del ordenador: "+ OrdenadorAString);
+
+    console.log("Has ganado");
+
+
+} else  if (Usuario === OrdenadorAString) {
+
+   console.log("Tu opción: "+ Usuario);
+
+   console.log("Elección del ordenador: "+ OrdenadorAString);
+
+   console.log("Habeis empatado");
+   
+
+} else {
+
+  console.log("Tu opción: "+ Usuario);
+
+  console.log("Elección del ordenador: "+ OrdenadorAString);
+
+  console.log("Has perdido");
+
+}
+
+}
+
+console.log(jugar("rock"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
